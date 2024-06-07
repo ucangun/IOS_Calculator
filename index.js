8cb63c343614726529f3dcd9900e647177685cc6
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-07 12:19:22 +0200

    functionality added to the action buttons, necessary contolls made

--- index.js
+++ index.js
@@ -1,24 +1,98 @@
 const container = document.querySelector(".container");
 const resultPage = document.querySelector(".result-page");
 const reset = document.querySelector(".boxReset");
-const absolute = document.querySelector(".boxAbsolute");
 
 //! Reset
 reset.addEventListener("click", () => {
   resultPage.textContent = "";
+  currentNumber = "";
+  previousNumber = "";
 });
+
+let clickedButton;
+let valueOfClickedButton;
+let operationButton;
+let currentNumber = "";
+let previousNumber = "";
+let result;
 
 //! Selecting of any buttons in container
 container.addEventListener("click", (event) => {
   clickedButton = event.target;
-  //console.log(clickedButton);
   valueOfClickedButton = clickedButton.textContent;
-  //console.log(valueOfClickedButton);
+  /// if the clicked element is a box
   if (clickedButton.classList.contains("box")) {
-    if (valueOfClickedButton >= 0 && valueOfClickedButton <= 9) {
-      resultPage.textContent = valueOfClickedButton;
-    } else if (isNaN(valueOfClickedButton) && valueOfClickedButton !== "AC") {
-      console.log(valueOfClickedButton);
+    /// if the value is a number
+    if (
+      !isNaN(valueOfClickedButton) &&
+      valueOfClickedButton >= 0 &&
+      valueOfClickedButton <= 9
+    ) {
+      currentNumber += valueOfClickedButton;
+      resultPage.textContent = currentNumber;
+      /// setting of current and previous numbers
+    } else if (
+      isNaN(valueOfClickedButton) &&
+      valueOfClickedButton !== "AC" &&
+      valueOfClickedButton !== "="
+    ) {
+      if (currentNumber !== "") {
+        previousNumber = currentNumber;
+        currentNumber = "";
+      }
+      operationButton = valueOfClickedButton;
+
+      /// if the value is =
+    } else if (valueOfClickedButton === "%" || valueOfClickedButton === "±") {
+      switch (operationButton) {
+        case "%":
+          if (currentNumber !== "") {
+            result = currentNumber / 100;
+            resultPage.textContent = result;
+          } else if (previousNumber !== "") {
+            result = previousNumber / 100;
+            resultPage.textContent = result;
+          }
+          break;
+
+        case "±":
+          result = previousNumber * -1;
+          resultPage.textContent = result;
+
+          break;
+
+        default:
+          break;
+      }
+    } else if (valueOfClickedButton === "=") {
+      if (currentNumber !== "" && previousNumber !== "" && operationButton) {
+        switch (operationButton) {
+          case "÷":
+            result = previousNumber / currentNumber;
+
+            break;
+
+          case "×":
+            result = previousNumber * currentNumber;
+
+            break;
+
+          case "+":
+            result = Number(previousNumber) + Number(currentNumber);
+
+            break;
+
+          case "-":
+            result = previousNumber - currentNumber;
+
+            break;
+          default:
+            break;
+        }
+        resultPage.textContent = result;
+        previousNumber = result;
+        currentNumber = "";
+      }
     }
   }
 });
