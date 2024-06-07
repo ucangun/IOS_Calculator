9b65bf4086eab61ad4ac28b6210473a9cda06cb8
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-07 14:10:17 +0200

    functionality added to the action buttons ( "." , "%", "±"), necessary contolls made

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ reset.addEventListener("click", () => {
   previousNumber = "";
 });
 
+//! Switch - Case Reset
+
+function afterSwitchCase() {
+  resultPage.textContent = result;
+  previousNumber = result;
+  currentNumber = "";
+}
+
 let clickedButton;
 let valueOfClickedButton;
 let operationButton;
@@ -30,11 +38,14 @@ container.addEventListener("click", (event) => {
     ) {
       currentNumber += valueOfClickedButton;
       resultPage.textContent = currentNumber;
-      /// setting of current and previous numbers
+      /// if the value is not a number ; setting of current and previous numbers
     } else if (
       isNaN(valueOfClickedButton) &&
       valueOfClickedButton !== "AC" &&
-      valueOfClickedButton !== "="
+      valueOfClickedButton !== "=" &&
+      valueOfClickedButton !== "%" &&
+      valueOfClickedButton !== "±" &&
+      valueOfClickedButton !== "."
     ) {
       if (currentNumber !== "") {
         previousNumber = currentNumber;
@@ -43,27 +54,6 @@ container.addEventListener("click", (event) => {
       operationButton = valueOfClickedButton;
 
       /// if the value is =
-    } else if (valueOfClickedButton === "%" || valueOfClickedButton === "±") {
-      switch (operationButton) {
-        case "%":
-          if (currentNumber !== "") {
-            result = currentNumber / 100;
-            resultPage.textContent = result;
-          } else if (previousNumber !== "") {
-            result = previousNumber / 100;
-            resultPage.textContent = result;
-          }
-          break;
-
-        case "±":
-          result = previousNumber * -1;
-          resultPage.textContent = result;
-
-          break;
-
-        default:
-          break;
-      }
     } else if (valueOfClickedButton === "=") {
       if (currentNumber !== "" && previousNumber !== "" && operationButton) {
         switch (operationButton) {
@@ -89,43 +79,34 @@ container.addEventListener("click", (event) => {
           default:
             break;
         }
-        resultPage.textContent = result;
-        previousNumber = result;
-        currentNumber = "";
+        afterSwitchCase();
+      }
+      /// if the value is % , . , ±
+    } else if (
+      valueOfClickedButton === "%" ||
+      valueOfClickedButton === "±" ||
+      valueOfClickedButton === "."
+    ) {
+      if (resultPage.textContent !== "") {
+        switch (valueOfClickedButton) {
+          case "%":
+            result = resultPage.textContent / 100;
+
+            break;
+
+          case "±":
+            result = resultPage.textContent * -1;
+            break;
+
+          case ".":
+            result = resultPage.textContent + ".";
+            break;
+
+          default:
+            break;
+        }
+        afterSwitchCase();
       }
     }
   }
 });
-
-//! Operation Buttons
-const divide = document.querySelector(".boxDivide");
-const procent = document.querySelector(".boxProcent");
-const multi = document.querySelector(".boxMulti");
-const minus = document.querySelector(".boxMinus");
-const plus = document.querySelector(".boxPlus");
-const equal = document.querySelector(".boxEqual");
-
-//*************************************************************/
-//! Numbers Selected && Show on Result Page
-/*
-const numbers = [];
-for (let i = 0; i <= 9; i++) {
-  numbers.push(`box${i}`);
-}
-//console.log(numbers);
-
-let selectedNumber = [];
-let valueOfSelectedNumber;
-
-numbers.forEach((number) => {
-  selectedNumber.push(document.querySelector(`.${number}`));
-});
-//console.log(selectedNumber);
-
-selectedNumber.forEach((activeNumber) => {
-  activeNumber.addEventListener("click", () => {
-    valueOfSelectedNumber = activeNumber.textContent;
-    resultPage.textContent = valueOfSelectedNumber;
-  });
-});
-*/
